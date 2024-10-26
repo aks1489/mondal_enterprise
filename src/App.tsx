eab77324aad75235fa7caf91141ed7b5e3f46a64
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-10-26 11:21:48 +0530

    #

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <div className='container-fluid'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
