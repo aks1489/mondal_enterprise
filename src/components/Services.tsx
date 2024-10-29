c6ee59d9c0d8f862df6be6445312cd9893d9b507
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-10-29 13:31:04 +0530

    services section

---
+++ src/components/Services.tsx
@@ -0,0 +1,51 @@
+export default function Services() {
+    return (
+        <section className="container">
+            <h1 className="text-center">Services</h1>
+            <hr />
+            <p className="fs-4 text-center">Comprehensive Manpower Solutions Tailored for Global Needs</p>
+            <div className="row gap-2 justify-content-center">
+                <div className="col-md-4">
+                    <div className="card" >
+                        <div className="card-body text-center">
+                            <h5 className="card-title">Skilled Labor Supply</h5>
+                            <p className="card-text">
+                                "We specialize in sourcing highly skilled professionals for sectors like construction, manufacturing, engineering, and more. Each candidate undergoes rigorous screening to meet industry standards."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="card" >
+                        <div className="card-body text-center">
+                            <h5 className="card-title">Unskilled Labor Supply</h5>
+                            <p className="card-text">
+                                "Our extensive network allows us to quickly meet demands for unskilled labor, providing dedicated and hardworking individuals who bring efficiency and productivity to every project."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="card" >
+                        <div className="card-body text-center">
+                            <h5 className="card-title">International Recruitment</h5>
+                            <p className="card-text">
+                                "We connect employers across various countries with qualified candidates, managing everything from hiring to visa processing, ensuring a smooth international placement experience."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="card" >
+                        <div className="card-body text-center">
+                            <h5 className="card-title">Customized Workforce Solutions</h5>
+                            <p className="card-text">
+                            "Our expert team works closely with each client to provide customized solutions that match their specific needs, ensuring flexibility, efficiency, and timely deployment of manpower."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
