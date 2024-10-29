6221c6ca6c9f6c0e1256988b2e96a5769e0a8aa5
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-10-29 13:30:22 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import './App.css'
+import Hero from './components/Hero.js'
 import NavBar from './components/NavBar'
+import Services from './components/Services.js'
 
 function App() {
 
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid m-0 p-0'>
       <NavBar />
+      <Hero />
+      <div className="container">
+        <hr className="border border-success border-1 opacity-50" />
+      </div>
+      <Services />
     </div>
   )
 }
